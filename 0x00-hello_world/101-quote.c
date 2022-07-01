5294243d9f9993b7d990f116d7ea060d7c43b8d0
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2022-06-30 18:59:59 -0700

    Fixed bug in task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 */
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - "
-		"Dora Korpar, 2015-10-19\n";
+	char message[] = "and that piece of art is useful\" - \
+Dora Korpar, 2015-10-19\n";
 
 	fwrite(message, strlen(message), sizeof(char), stderr);
 	return (1);
