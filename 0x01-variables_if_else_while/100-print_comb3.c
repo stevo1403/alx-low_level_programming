e1448c14e3554154868c1dd980e7c676fd3e3247
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2022-07-01 08:14:49 -0700

    Completed task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point for the application.
+ *
+ * Return: The program exit code(0)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if ( i > j || i == j)
+				continue;
+
+			putchar(i);
+			putchar(j);
+
+			if (i != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
