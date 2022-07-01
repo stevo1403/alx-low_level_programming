5abf1a00d5bdcd077e03f4c7e9f7aeccd3ab3c86
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2022-06-30 18:30:56 -0700

    Fixed bugs in task 5 & 6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char message[] = "with proper grammar, but the outcome is a piece of art,";
 
-	printf("%s", message);
+	printf("%s\n", message);
 	return (0);
 }
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int lln = 500 * 500;
 	float fn = 500.5;
 
-	printf("Size of a char: %lu bytes(s)\n", sizeof(c));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(n));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(ln));
-	printf("Size of a long long int: %lu bytes(s)\n", sizeof(lln));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(fn));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(n));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(ln));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(lln));
+	printf("Size of a float: %lu byte(s)\n", sizeof(fn));
 	return (0);
 }
 
