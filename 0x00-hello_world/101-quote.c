394d7e76abb017267ae7a7ad6b15e80e9141fd7e
Adebayo Stephen <34807365+stevo1403@users.noreply.github.com>
2022-06-30 18:57:30 -0700

    Completed tasks 7 and 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: The program's exit code. Returns 0 indicating success.
+*/
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - "
+		"Dora Korpar, 2015-10-19\n";
+
+	fwrite(message, strlen(message), sizeof(char), stderr);
+	return (1);
+}
+
